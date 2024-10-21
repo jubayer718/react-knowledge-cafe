aa73ae27a430c8c64990dfc13ee9e81a6acd9e8a
jubayer718 <jubayerahmed2462@gmail.com>
2024-10-21 23:01:59 +0700

    data fetch complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 
 import './App.css'
+import Blogs from './components/Blog/Blogs'
+import Header from './components/Header/Header'
 
 function App() {
   
@@ -7,7 +9,8 @@ function App() {
   return (
     <>
   
-      <h1 className='bg-gray-300 p-5 text-3xl font-bold text-center'>Vite + React</h1>
+      <Header></Header>
+      <Blogs></Blogs>
    
     </>
   )
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+  return (
+    <div>
+       <h1 className=' p-5 text-3xl font-bold text-center'>Knowledge cafe</h1>
+    </div>
+  );
+};
+
+export default Header;
