dc51c14780c63e055874b0a79e908fcee5610bd5
jubayer718 <jubayerahmed2462@gmail.com>
2024-10-21 21:26:40 +0700

    tailwind added with react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+  
+      <h1 className='bg-gray-300 p-5 text-3xl font-bold text-center'>Vite + React</h1>
+   
+    </>
+  )
+}
+
+export default App
